7dda0728f28c91af99474f3dbe8a5ea42d4f377d
IGORTB\Igor Tavares Barata <igor.meier@gmail.com>
2019-04-14 22:28:38 -0300

    Desenvolvimento do objeto cliente

---
+++ Stone.Clients.Models/Entities/Client.cs
@@ -0,0 +1,16 @@
+﻿namespace Stone.Clients.Models.Entities
+{
+    public class Client
+    {
+        public Client(string name, string state, string cpf)
+        {
+            Name = name;
+            State = state;
+            Cpf = cpf;
+        }
+
+        public string Name { get; private set; }
+        public string State { get; private set; }
+        public string Cpf { get; private set; }
+    }
+}
